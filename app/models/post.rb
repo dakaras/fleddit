0f446ad608917e70184e8eae533a473a66f3982d
github username <github email address>
2018-12-27 11:29:34 -0600

    fleddit session 1

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  has_many :votes
+  has_many :users, through: :votes
+  belongs_to :user, foreign_key: "author_id"
+
+  validates :title, presence: true
+  validates :content, presence: true
+  scope :ordered_by_votes, -> {joins(:votes).group('posts.id').order('SUM("votes.value")')}
+end
---
+++ app/models/vote.rb
@@ -0,0 +1,7 @@
+class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :value, presence: true, inclusion: { in: [-1, 1],
+  message: "Your vote must be either 1 or -1."}
+end
